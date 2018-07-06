28979116a29cb10faf3595cdab2ff09c3a15d95a
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-06 23:18:19 +0530

    DAO !

---
+++ StudentInformation/src/com/dao/MyDataDAO.java
@@ -0,0 +1,14 @@
+package com.dao;
+
+import com.model.MyData;
+import java.util.List;
+
+public interface MyDataDAO {
+
+  public boolean addUser(MyData m);
+  //public List<MyData> getAllData();
+  //public boolean deleteData(int rollno);
+  //public boolean deleteAllData();
+  //public boolean updateData(int rollno,String firstname);
+  //public MyData findData(int rollno);
+}
