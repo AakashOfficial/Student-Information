5ecef11962ac00acb4b616b23d95a4e9b8f4adcb
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-08 08:25:51 +0530

    File For DAO !

--- StudentInformation/src/com/dao/MyDataDAO.java
+++ StudentInformation/src/com/dao/MyDataDAO.java
@@ -1,11 +1,13 @@
 package com.dao;
 
+import com.model.HigherAuthorities;
 import com.model.MyData;
-import java.util.List;
+//import java.util.List;
 
 public interface MyDataDAO {
 
   public boolean addUser(MyData m);
+  public boolean addAuthorities(HigherAuthorities ha);
   //public List<MyData> getAllData();
   //public boolean deleteData(int rollno);
   //public boolean deleteAllData();
