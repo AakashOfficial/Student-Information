a447dea0acac1cadcee7c6f1200dd3fa14859df9
Aakash Tyagi <aakashtyagi1996@outlook.com>
2018-07-08 08:30:41 +0530

    For Adding Higher Authorities !

---
+++ StudentInformation/src/com/model/HigherAuthorities.java
@@ -0,0 +1,48 @@
+package com.model;
+
+public class HigherAuthorities {
+
+	String id;
+	String password;
+	String role;
+	String name;
+	String address;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getRole() {
+		return role;
+	}
+	public void setRole(String role) {
+		this.role = role;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	@Override
+	public String toString() {
+		return "HigherAuthorities [id=" + id + ", password=" + password + ", role=" + role + ", name=" + name
+				+ ", address=" + address + "]";
+	}
+		
+}
